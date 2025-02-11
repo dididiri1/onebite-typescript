46b7ab09466f80287070f019aab39f9feb9f168f
dididiri1 <dididiri1@naver.com>
2025-02-11 10:57:42 +0900

    docs: any와 unknown

---
+++ section2/src/chapter6.ts
@@ -0,0 +1,23 @@
+// any
+let anyVar: any = 10;
+anyVar = "hello";
+
+anyVar = true;
+anyVar = {};
+
+anyVar.toUpperCase();
+anyVar.toFixed();
+anyVar.a;
+
+let num: number = 10;
+
+// unknown
+let unknownVar: unknown;
+
+unknownVar = "";
+unknownVar = 1;
+unknownVar = () => {};
+
+if (typeof unknownVar === "number") {
+  num = unknownVar;
+}
