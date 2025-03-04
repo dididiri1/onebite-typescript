6fdfaec7476a08eaa1cbd328572f331a88d36d35
dididiri1 <dididiri1@naver.com>
2025-03-04 12:20:51 +0900

    docs: 타입 스크립트 리엑트  - 1. 상태관리와 Props 1

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,23 +1,34 @@
-import logo from "./logo.svg";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd} />
     </div>
   );
 }
