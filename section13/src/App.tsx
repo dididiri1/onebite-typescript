f242cbe573cf87c285d9dbb07edaf058fe39e236
dididiri1 <dididiri1@naver.com>
2025-03-15 12:41:55 +0900

    feat: DiaryList 컴포넌트 추가

--- section13/src/App.tsx
+++ section13/src/App.tsx
@@ -136,35 +136,6 @@ function App() {
 
   return (
     <div className="App">
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      ></Header>
-
-      <button
-        onClick={() => {
-          onCreate(new Date().getTime(), 1, "Hello");
-        }}
-      >
-        일기 추가
-      </button>
-
-      <button
-        onClick={() => {
-          onUpdate(1, new Date().getTime(), 1, "수정된 일기힙니다.");
-        }}
-      >
-        일기 수정
-      </button>
-
-      <button
-        onClick={() => {
-          onDelete(1);
-        }}
-      >
-        일기 삭제
-      </button>
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext.Provider
           value={{
